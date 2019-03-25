124757a0182651d9418effb728063405ea52046d
Simran <simran_palia@yahoo.co.in>
2019-03-25 14:20:20 +0800

    delegate v1 example snippet.

--- DelegateSampleApp/Program.cs
+++ DelegateSampleApp/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace DelegateSampleApp
 {
+
     class Program
     {
+        public delegate int AddNumbers(int a, int b);
+
+        public class TestClass
+        {
+            public void addNumbersVoid(int n1, int n2)
+            {
+                int res = n1 + n2;
+            }
+
+            public int addNumbersInt(int n1, int n2)
+            {
+                return n1 + n2;
+            }
+        }
+
         static void Main(string[] args)
         {
+            AddNumbers addNumbersObj = new AddNumbers(new TestClass().addNumbersInt);
+            Console.WriteLine(addNumbersObj(5, 5));
+            Console.ReadLine();
         }
     }
 }
